62f333006de1deed41a2a427e560556902dec262
Seif-01 <sifreyassin@gmail.com>
2024-03-01 16:13:46 -0400

    betty check 1

---
+++ t1_betty.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * beautiful code that passes the Betty checks
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int count;
+
+    for (count = 0; count < 10; count++)
+    {
+        printf("the count is %d\n", count);
+    }
+
+    return 0;
+}
