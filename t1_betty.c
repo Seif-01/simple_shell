88d005519f746292bbdd7d1ffa9b630bd554dd30
Seif-01 <sifreyassin@gmail.com>
2024-03-01 17:02:00 -0400

    fix task 1

--- t1_betty.c
+++ t1_betty.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 
 /**
- * beautiful code that passes the Betty checks
- * main - Entry point of the program
- *
- * Return: Always 0 (Success)
- */
+* beautiful code that passes the Betty checks
+* main - Entry point of the program
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-    int count;
-
-    for (count = 0; count < 10; count++)
-    {
-        printf("the count is %d\n", count);
-    }
-
-    return 0;
+int count;
+for (count = 0; count < 10; count++)
+{
+printf("the count is %d\n", count);
+}
+return 0;
 }
